c4940939da91dcaead655ed4ed7bb7200533fa5d
riadhGharbiTFG <riadh@tixforgigs.com>
2024-07-10 09:05:52 +0100

    document core package

--- lib/bestwaytoproceed.dart
+++ lib/bestwaytoproceed.dart
@@ -9,43 +9,44 @@ import 'package:image_picker/image_picker.dart';
 
 import 'models/way_data.dart';
 
+/// A class that compares images to analyze and determine the safety
+/// of a given scene for a blind person to proceed.
 class ImageComparison {
+  /// The API key for the generative AI model.
   final String apiKey;
 
+  /// Constructs an instance of [ImageComparison] with the provided [apiKey].
   ImageComparison(this.apiKey);
 
-  Future<WayData?> compareImages({required XFile images}) async {
-    // log('selecting the first image');
-    // final XFile? firstImageFile = await _picker.pickImage(source: ImageSource.gallery);
-    // log("selecting the second image ");
-    // final XFile? secondImageFile = await _picker.pickImage(source: ImageSource.gallery);
-
+  /// Compares the provided image and returns the analysis as [WayData].
+  ///
+  /// The analysis includes factors such as the presence of clear pathways,
+  /// absence of obstacles, adequate lighting, and other elements affecting safety
+  /// and ease of navigation.
+  ///
+  /// Returns `null` if an error occurs during the process.
+  ///
+  /// [image] is the image file to be analyzed.
+  Future<WayData?> compareImages({required XFile image}) async {
     try {
-      //  if (firstImageFile == null || secondImageFile == null) {
-      //    print('Image selection canceled');
-      //    return;
-      //  }
-
-      final image = await File(images.path).readAsBytes();
-      //final secondImage = await File(images.last.path).readAsBytes();
+      final imageResult = await File(image.path).readAsBytes();
 
       final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey);
 
       final prompt = TextPart(
           "Analyze the provided images and determine if the scene represents a safe and appropriate time for a blind person to proceed. "
-          "Consider the presence of clear pathways, absence of obstacles, adequate lighting, and any other elements that might affect safety and ease of navigation."
-          "Consider factors such as the presence of clear pathways, absence of obstacles, adequate lighting, and any other elements that might affect safety and ease of navigation. "
-          "Please provide a detailed explanation of your analysis."
-          "provide the percentage of safety for the next 5 meters"
-          "describe the best way to proceed in short phrase"
-          "Consider whether the road is intended for pedestrians or vehicles."
-          "suggest me an advice for government to improve indications "
-          "suggest an idea for government improvements that would likely cost less than 100 USD?"
-          "suggest me an advice to improve the accuracy of the output for the next use to add it here"
-          "return all data only in developed json and remove json word and keep this form as a model ${jsonEncode(const WayData(safetyPercentage: 40, proceedPhrase: "example", roadType: "example", governmentAdvice: 'example', lowCostImprovement: "example", accuracyImprovement: "example").toJson())}");
-      //and keep this as a model {safety_percentage: 1 best_way_to_proceed: Use a cane or guide dog to navigate the uneven terrain., road_type : Natural pathway, phrase_improvement: Caution: Uneven terrain and potential for falls.}
+          "Consider the presence of clear pathways, absence of obstacles, adequate lighting, and any other elements that might affect safety and ease of navigation. "
+          "Please provide a detailed explanation of your analysis. "
+          "Provide the percentage of safety for the next 5 meters. "
+          "Describe the best way to proceed in a short phrase. "
+          "Consider whether the road is intended for pedestrians or vehicles. "
+          "Suggest an advice for the government to improve indications. "
+          "Suggest an idea for government improvements that would likely cost less than 100 USD. "
+          "Suggest an advice to improve the accuracy of the output for the next use to add it here. "
+          "Return all data only in a developed JSON format and remove the JSON word, keeping this form as a model ${jsonEncode(const WayData(safetyPercentage: 40, proceedPhrase: 'example', roadType: 'example', governmentAdvice: 'example', lowCostImprovement: 'example', accuracyImprovement: 'example').toJson())}.");
+
       final imageParts = [
-        DataPart('image/jpeg', image),
+        DataPart('image/jpeg', imageResult),
       ];
 
       final response = await model.generateContent([
@@ -53,11 +54,11 @@ class ImageComparison {
       ]);
       final result = response.text;
       final jsonResult = jsonDecode(result!);
-      log('the json result: $jsonResult');
+      log('The JSON result: $jsonResult');
       final modelResult = WayData.fromJson(jsonResult);
       return modelResult;
     } catch (e, s) {
-      log("error: $e, stack: $s");
+      log('Error: $e, stack: $s');
       return null;
     }
   }
